2e97ccc9b6974439e2404e07f9917dc5de8b3ffb
Ivan Jacob Agaloos Pesigan <jeksterslab@users.noreply.github.com>
2023-06-18 11:18:37 +0000

    MASTER

--- src/custom.sh
+++ src/custom.sh
@@ -2,19 +2,6 @@
 
 set -e
 
-# Solution to hard-coded Makevars in the arma branch
-R -e "                      \
-    install.packages(       \
-        c(                  \
-          'Rcpp',           \
-          'RcppArmadillo',  \
-          'RcppGSL'         \
-        ),                  \
-        repos = 'https://packagemanager.rstudio.com/all/__linux__/jammy/latest', \
-        lib = file.path(Sys.getenv('R_HOME'), 'library')                         \
-    )                                                                            \
-"
-
 # Solution to documentation issues
 R -e "                                                    \
     remotes::install_version(                             \
@@ -25,7 +12,7 @@ R -e "                                                    \
 "
 
 # dynr
-git clone -b arma https://github.com/mhunter1/dynr.git
+git clone https://github.com/mhunter1/dynr.git
 cd dynr
 ./configure
 make clean install
@@ -49,11 +36,11 @@ echo "session-default-new-project-dir=/home/${DEFAULT_USER}/project-dir" >> /etc
 chown -R "${DEFAULT_USER}:${DEFAULT_USER}" "/home/${DEFAULT_USER}/project-dir"
 
 ## build
-echo "$(git ls-remote https://github.com/mhunter1/dynr.git arma)" > /etc/profile.d/container_init.sh
+echo "$(git ls-remote https://github.com/mhunter1/dynr.git master)" > /etc/profile.d/container_init.sh
 awk '{print $1 > "/etc/profile.d/container_init.sh"}' /etc/profile.d/container_init.sh
 CONTAINER_RELEASE=$(cat /etc/profile.d/container_init.sh)
 echo "export CONTAINER_RELEASE=$CONTAINER_RELEASE" > /etc/profile.d/container_init.sh
-CONTAINER_RELEASE_MSG="\"This release is based on the commit $CONTAINER_RELEASE.\""
+CONTAINER_RELEASE_MSG="\"This release is based on commit $CONTAINER_RELEASE in the master branch of mhunter1/dynr.\""
 echo "export CONTAINER_RELEASE_MSG=$CONTAINER_RELEASE_MSG" >> /etc/profile.d/container_init.sh
 
 mkdir -p /srv/build
